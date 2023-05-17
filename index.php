87d95a040643fe90225defe5c9cd8465c409c7fa
paulo08062000 <paulo.henriquefracassoda.silva@escola.pr.gov.br>
2023-05-17 10:37:13 -0300

    new file:   index.php

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+$pesquisa = "Smart TV Philco PTV32G70RCH LED Roku OS HD 32 110V/220V";
+error_reporting(E_ALL ^ E_DEPRECATED);
+// definições de host, database, usuário e senha
+$host = "sql208.epizy.com";
+$db   = "epiz_33750471_aula4tii";
+$user = "epiz_33750471";
+$pass = "Q5dSCPqDru4tKM";
+// conecta ao banco de dados
+$con = mysql_pconnect($host, $user, $pass) or trigger_error(mysql_error(),E_USER_ERROR);
+// seleciona a base de dados em que vamos trabalhar
+mysql_select_db($db, $con);
+// cria a instrução SQL que vai selecionar os dados
+$query = sprintf("SELECT identificador, nome, valor, foto FROM cadastro  ");
+// executa a query
+$dados = mysql_query($query, $con) or die(mysql_error());
+// transforma os dados em um array
+$linha = mysql_fetch_assoc($dados);
+// calcula quantos dados retornaram
+$total = mysql_num_rows($dados);
+?>
+
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <div class="container-fluid">
+	<div class="row">
+  <?php
+	// se o número de resultados for maior que zero, mostra os dados
+	if($total > 0) {
+		// inicia o loop que vai mostrar todos os dados
+		do {
+?>
+
+
+	<div class="card" style="width: 18rem;">
+  <img src="<?=$linha['foto']?>" class="card-img-top" alt="...">
+  <ul class="list-group list-group-flush">
+  	<li class="list-group-item"><b>Nome:</b> <?=$linha['nome']?> </li>
+    <li class="list-group-item"><b>Valor:</b><?=$linha['valor']?> </li>
+    <li class="list-group-item"> Cliente:<b> <?=$linha['identificador']?></b></li>
+  </ul>
+  </div>
+  
+
+
+
+
+
+
+
+
+			
+<?php
+		// finaliza o loop que vai mostrar os dados
+		}while($linha = mysql_fetch_assoc($dados));
+	// fim do if
+	}
+?>
+  </div></div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+<?php
+// tira o resultado da busca da memória
+mysql_free_result($dados);
+?>
